b209ffe475b7b66dc15af4e8340f6e64f89e7f14
Cameron Moeller <cameron.moeller@gmail.com>
2016-12-06 16:02:04 -0500

    initial2 heroku

--- questionbox/wsgi.py
+++ questionbox/wsgi.py
@@ -4,7 +4,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "questionbox.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
