502eebab76dcf29106db0b1dbfad6dd8fbf88c4c
Cameron Moeller <cameron.moeller@gmail.com>
2016-12-07 13:31:23 -0500

    testing more

--- stack/static/js/stack.js
+++ stack/static/js/stack.js
@@ -1,13 +1,12 @@
 function testQuestion(){
     var $stuff = $("<ol>")
-    for(var j = 1; j<10; j++){
-        jQuery.ajax("https://lit-escarpment-88574.herokuapp.com/stack/questions/").done(function(results){
-            var questionStuff = results.results
-            for(var i = 0; i < questionStuff.length; i++){
-                $stuff.html($stuff.html()+ questionStuff[i]['title'] + "<br>")
-                $("#test").append($stuff)
-            }
-        })
-    }
+    jQuery.ajax("https://lit-escarpment-88574.herokuapp.com/stack/questions/").done(function(results){
+        var questionStuff = results.results
+        console.log(results.results)
+        for(var i = 0; i < questionStuff.length; i++){
+            $stuff.html($stuff.html()+ questionStuff[i]['title'] + "<br>")
+            $("#test").append($stuff)
+        }
+    })
 }
 testQuestion()
