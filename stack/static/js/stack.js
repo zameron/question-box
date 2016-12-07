ca2af248e03c8ed66a354c5c403a9d36288a9766
Cameron Moeller <cameron.moeller@gmail.com>
2016-12-07 13:36:36 -0500

    testing more

--- stack/static/js/stack.js
+++ stack/static/js/stack.js
@@ -1,3 +1,13 @@
+
+function questionPost(){
+    var postdata = {'title': 'test2q', 'description': 'testDES2'}
+    jQuery.ajax({url:'/stack/questions', data:postdata, type:'POST'
+    }).done(function(){
+        location = location
+    })
+}
+questionPost()
+
 function testQuestion(){
     var $stuff = $("<ol>")
     jQuery.ajax("https://lit-escarpment-88574.herokuapp.com/stack/questions/").done(function(results){
